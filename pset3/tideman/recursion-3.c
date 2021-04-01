c44ecec1575050c89bc37e5af4d259273592cbb8
Nicolas Severyns <nicolasseveryns@gmail.com>
2021-04-01 19:52:13 +0000

    Recursion-3 finally works. Scoping of varsgit status

--- pset3/tideman/recursion-3.c
+++ pset3/tideman/recursion-3.c
@@ -9,12 +9,12 @@ typedef struct
 }
 pair;
 const int PAIRCOUNT = 10;
-// pair pairs[PAIRCOUNT] = {{1,2},{1,4},{3,2}, {1,3},{3,4},{2,3}}; // final cycle
+//pair pairs[PAIRCOUNT] = {{1,2},{1,4},{3,2}, {1,3},{3,4},{2,3}}; // final cycle 6
 // pair pairs[PAIRCOUNT] = {{1,2},{2,3},{3,4},{4,3},{4,5},{5,1},{2,5},{1,6},{4,1}}; // badpairs
 pair pairs[PAIRCOUNT] = {{1,2},{2,3},{3,4},{4,3},{4,5},{5,1},{2,5},{1,6},{4,1},{6,1}}; // badpairs final cycle 10
 //pair pairs[PAIRCOUNT] = {{0,1},{1,2},{2,3},{3,2},{2,0}};
 // pair pairs[PAIRCOUNT] = {{1,2},{2,3},{4,3},{5,4},{1,5},{6,1},{6,4}}; // goodpairs
-bool cycle(int winner, int loser, int locked_count, int selected_pairs[], bool visited[]);
+bool cycle(int winner, int loser, int locked_count, int selected_pairs[], bool visited[], bool flag, int original_loser);
 
 int main ()
 {
@@ -24,11 +24,13 @@ int main ()
     for (int i = 0; i < PAIRCOUNT ; i++)
     {
         bool visited[PAIRCOUNT]; // nieuw
+        bool flag = false;
+        int original_loser = pairs[i].loser;
         for (int j = 0; j < PAIRCOUNT; j++)
         {
                 visited[j] = false;
         }
-        if (!cycle(pairs[i].winner, pairs[i].loser, locked_count, selected_pairs, visited))
+        if (!cycle(pairs[i].winner, pairs[i].loser, locked_count, selected_pairs, visited, flag, original_loser))
         {
            printf("Hoera! Paar %d, %d is goed!\n", pairs[i].winner, pairs[i].loser);
             selected_pairs[locked_count] = i;
@@ -41,16 +43,16 @@ int main ()
 
 // does loser win a locked pair?
 // move along path
-bool cycle(int winner, int loser, int count, int selected_pairs[], bool visited[])
+bool cycle(int winner, int loser, int count, int selected_pairs[], bool visited[], bool flag, int original_loser)
 {
     bool is_winner = false;
     int current_winner;
     int current_loser;
     int current_pair;
     if (count == 0)
-    {
-        return false;
-        // first pair is always ok.
+   {
+      return false;
+       // first pair is always ok.
     }
     for (int i = 0; i < count; i++)
     {
@@ -65,6 +67,7 @@ bool cycle(int winner, int loser, int count, int selected_pairs[], bool visited[
         {
             visited[current_pair] = true; // nieuw that should be number of pair
             is_winner = true;
+            break;
         }
     }
         if (is_winner == true && winner == current_loser)
@@ -72,24 +75,24 @@ bool cycle(int winner, int loser, int count, int selected_pairs[], bool visited[
             return true;
             // if those two are the same, we've detected a cycle and exit.
         }
-        else if (is_winner == true && current_loser == loser) // voorwaarde toegevoegd voor middencase
+        else if (is_winner == true) // voorwaarde toegevoegd voor middencase
         {
-           // deze hier staat in voor mijn middenste casus. Spijtig genoeg heb ik
-           // mijn code van vandaag weggegooid...
-                // TODO CHECK WHATS WRONG!
                 // here we should keep making those links with the follo
                 // wing losers, maybe we find a cycle
-          return cycle(winner, current_loser, count , selected_pairs, visited);
+            // but if it doesn't find what it looks for on a first run, a second one, or a third might be needed...
+            flag = true;
+          return cycle(winner, current_loser, count , selected_pairs, visited, flag, original_loser);
         }
-         else if (is_winner == true)
+        else if (flag == true)
         {
+            flag = false;
             // but if the loop found a winner, but didn't create a cycle, we should
             // check if there's a second or more winner we overlooked...
-            return cycle(winner, loser, count, selected_pairs, visited);
+            return cycle(winner, original_loser, count, selected_pairs, visited, flag, original_loser); // TIJDELIJK UIT
         }
-       else // if (is_winner == false)
+        else
         {
-            // if all else fails, we have a winner... I think
+          // no options left
             return false;
         }
 }
