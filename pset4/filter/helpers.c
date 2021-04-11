fe3cb26a07a6366da75158ef4a4b2cafe1fd3cd4
Nicolas Severyns <nicolasseveryns@gmail.com>
2021-04-11 17:42:19 +0000

    Started working on BLUR. Think I've made a logical mistake somewhere.

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,11 +1,12 @@
 #include "helpers.h"
 #include <math.h>
 #include <stdlib.h>
-
+// helper function
+void average_surrounding_pixels(RGBTRIPLE image[h][v], int c, int h, int v, int c2);
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-   // for every pixe (RGBTRIPLE) in image
+   // for every pixel (RGBTRIPLE) in image
    for (int i = 0; i < height ; i++)
    {
        for (int j = 0; j < width; j++)
@@ -50,11 +51,52 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // for loop for every pixel
+    // a list of conditions
+    // in every condition a call to average_surrounding_pixels
+    // with the correct counters and offsets.
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    return;
+}
+// helper function to calculate averages and assign them to the pixels
+void average_surrounding_pixels(int c, int h, int v, int c2) // ik moet de pixel doorgeven aan de functie!!!!
+// hierover nadenken
+// TODO: ofwel in de condities de verschillende h v en c toewijzen!! en deze functie niet gebruiken.
+{
+    float all_red = 0, all_blue = 0, all_green = 0;
+    int count = 0;
+    for (int i = h; i <= c; i++)
+    {
+        for (int j = v, j <= c2; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            all_red = pixel.rgbtRed + all_red;
+            all_blue = pixel.rgbtBlue + all_blue;
+            all_green = pixel.rgbtGreen + all_green;
+            count ++;
+        }
+    }
+    // assigning should also be a for loop, because we need the averages
+    int average_blue = roundf(all_blue / (float)count);
+    int average_red = roundf(all_red) / (float)count);
+    int average_green = roundf(all_green) / (float)count);
+
+    for (int i = h; i <= c; i++)
+    {
+        RGBTRIPLE* pixelrow = image[i];
+        for (int j = v, j <= c2; j++)
+        {
+            // assign every pixel color its average
+            // make a loop over an array with RGB?
+            pixelrow[j].rgbtRed = average_red;
+            pixelrow[j].rgbtBlue = average_blue;
+            pixelrow[j].rgbtGreen = average_green;
+        }
+    }
     return;
 }
